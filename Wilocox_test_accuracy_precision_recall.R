a798cb9ee320ba2b52479c7f106b43d3c67fe5cb
lucy2611 <62658632+lucy2611@users.noreply.github.com>
2023-02-13 16:19:57 +0000

    Add files via upload

---
+++ Wilocox_test_accuracy_precision_recall.R
@@ -0,0 +1,50 @@
+library(dplyr)
+
+data<- read.csv("Accuracy_precision_recall_vals_r.csv")
+
+pairwise_wilcox_test <- function(data, column, groups){
+  
+  for (i in 1:length(groups)){
+    sub_data <- subset(data, Method %in% groups[[i]])
+    result <- wilcox.test(as.formula(paste(column, "~ Method")), data = sub_data,
+                          exact = FALSE)
+    print(paste("Comparison between", groups[[i]][[1]], "and", groups[[i]][[2]], ":", sep = " "))
+    print(result)
+    print("\n")
+  }
+}
+
+pairwise_wilcox_test(data, "Accuracy", list(c("OG_RGI", "LR"),
+                                            c("OG_RGI", "RGI_specific"),
+                                            c("OG_RGI", "RGI_all"),
+                                            c("OG_RGI", "Eggnog"),
+                                            c("LR", "RGI_specific"),
+                                            c("LR", "RGI_all"),
+                                            c("LR", "Eggnog"),
+                                            c("RGI_specific", "RGI_all"),
+                                            c("RGI_specific", "Eggnog"),
+                                            c("RGI_all", "Eggnog")))
+
+pairwise_wilcox_test(data, "average_precision", list(c("OG_RGI", "LR"),
+                                            c("OG_RGI", "RGI_specific"),
+                                            c("OG_RGI", "RGI_all"),
+                                            c("OG_RGI", "Eggnog"),
+                                            c("LR", "RGI_specific"),
+                                            c("LR", "RGI_all"),
+                                            c("LR", "Eggnog"),
+                                            c("RGI_specific", "RGI_all"),
+                                            c("RGI_specific", "Eggnog"),
+                                            c("RGI_all", "Eggnog")))
+
+pairwise_wilcox_test(data, "average_recall", list(c("OG_RGI", "LR"),
+                                            c("OG_RGI", "RGI_specific"),
+                                            c("OG_RGI", "RGI_all"),
+                                            c("OG_RGI", "Eggnog"),
+                                            c("LR", "RGI_specific"),
+                                            c("LR", "RGI_all"),
+                                            c("LR", "Eggnog"),
+                                            c("RGI_specific", "RGI_all"),
+                                            c("RGI_specific", "Eggnog"),
+                                            c("RGI_all", "Eggnog")))
+
+
