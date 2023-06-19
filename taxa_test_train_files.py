aa90eda7a2125bfb2b4e9476ce2568e8b45ffa83
LucyDillon <62658632+LucyDillon@users.noreply.github.com>
2023-06-19 17:23:00 +0100

    Update taxa_test_train_files.py

--- taxa_test_train_files.py
+++ taxa_test_train_files.py
@@ -71,44 +71,45 @@ final_table_reduced = final_table[["genome_id", "Gene", "GeneHit", "antibiotic",
 # only keep rows with susceptible or res phenotype:
 final_table = final_table_reduced[(final_table_reduced["add_phenotype"] == 'Susceptible') | (final_table_reduced["add_phenotype"] == 'Resistant')]
 
-taxonomy_info_2 = pd.merge(left=final_table, right=Species, on='genome_id', how='inner')
+# Convert into a table:
+table = pd.pivot_table(final_table, values=['GeneHit'], index=['genome_id'],
+                    columns=['Gene'], fill_value=0)
 
-taxonomy_info_2 = taxonomy_info_2.drop_duplicates()
+table = table.reset_index()
+table.columns = table.columns.droplevel(0)
+table.rename(columns = {list(table)[0]: 'genome_id'}, inplace = True)
 
-Genes = data['Gene']
+# Add genus to the table:
+taxonomy_table = pd.merge(left=table, right=Species, on='genome_id')
+# drop duplicates:
+taxonomy_table = taxonomy_info_2_1.drop_duplicates()
+
+# Get phenotype info ready to merge with the table above:
+pheno = final_table[['genome_id', 'antibiotic', 'add_phenotype']] 
+pheno = pheno.drop_duplicates()
+
+#merge phenotype data with RGI data
+taxonomy_pheno_table= pd.merge(left=taxonomy_table, right=pheno, on='genome_id')
+
+#Get a list of the unique genes:
+Genes = taxonomy_info_2['Gene']
 Genes = Genes.sort_values(ascending=True)
 Genes = Genes.drop_duplicates()
 
-# Convert into a table:
-table = pd.pivot_table(taxonomy_info_2, values=['GeneHit'], index=['genome_id'],
-                    columns=['Gene'], fill_value=0)
-
-# merge table and phenotype and Genus:
-taxa_pheno = taxonomy_info_2.drop('Gene', 1)
-taxa_pheno = taxa_pheno.drop('GeneHit', 1)
-
-taxa_pheno = taxa_pheno.drop_duplicates() 
-
-table = table.drop_duplicates()
-
-taxa_pheno =taxa_pheno.set_index('genome_id')
-#merge phenotype data with eggnog data
-taxonomy_info_2 = table.join(taxa_pheno)
 
 #Make a arff file for each genus:            
-def write_to_arff_genus(antibiotic, genus, taxonomy_info_2):
-    antibiotic_df = taxonomy_info_2[taxonomy_info_2['antibiotic'] == antibiotic]
-    antibiotic_df = taxonomy_info_2[taxonomy_info_2['Genus'] == genus]
-    antibiotic_df = antibiotic_df.reset_index()
+def write_to_arff_genus(antibiotic, genu):
+    antibiotic_df = taxonomy_pheno_table[taxonomy_pheno_table['antibiotic'] == antibiotic]
+    antibiotic_df = taxonomy_pheno_table[taxonomy_pheno_table['Genus'] == genus]
     antibiotic_df = antibiotic_df.drop_duplicates()
     antibiotic_df = antibiotic_df.set_index('genome_id')
-    antibiotic_df = antibiotic_df.drop('antibiotic', 1)
-    antibiotic_df = antibiotic_df.drop('Genus', 1)
+    antibiotic_df = antibiotic_df.drop('antibiotic', axis=1)
+    antibiotic_df = antibiotic_df.drop('Genus', axis=1)
     antibiotic_df_array = antibiotic_df.to_string(index=False, header=False)
     antibiotic_df_array_sep = antibiotic_df_array.replace(' ', ',')
     antibiotic_df_array_sep = antibiotic_df_array_sep.replace(',,,', ',')
     antibiotic_df_array_sep = antibiotic_df_array_sep.replace(',,', ',')
-    outfile_genus = f"{antibiotic}_{genus}_taxa_files_TEST_TEST.arff"
+    outfile_genus = f"{antibiotic}_{genus}_taxa_files.arff"
     att_out_genus = open(outfile_genus, "w")
     i = 'Gene'
     att_out_genus.write('@RELATION    ' + i + '\n')
@@ -132,23 +133,23 @@ genuses = ['Achromobacter', 'Acinetobacter', 'Campylobacter', 'Citrobacter',
 
 for antibiotic in antibiotics:
     for genus in genuses:
-        write_to_arff_genus(antibiotic, genus, taxonomy_info_2)
+        write_to_arff_genus(antibiotic, genus)
         
+
 # Make a arff file that is all but one genus:
 
-def write_to_arff_except_genus(antibiotic, genus, taxonomy_info_2):
-    antibiotic_df = taxonomy_info_2[taxonomy_info_2['antibiotic'] == antibiotic]
-    antibiotic_df = taxonomy_info_2[taxonomy_info_2['Genus'] != genus]
-    antibiotic_df = antibiotic_df.reset_index()
+def write_to_arff_except_genus(antibiotic, genus):
+    antibiotic_df = taxonomy_pheno_table[taxonomy_pheno_table['antibiotic'] == antibiotic]
+    antibiotic_df = taxonomy_pheno_table[taxonomy_pheno_table['Genus'] != genus]
     antibiotic_df = antibiotic_df.drop_duplicates()
     antibiotic_df = antibiotic_df.set_index('genome_id')
-    antibiotic_df = antibiotic_df.drop('antibiotic', 1)
-    antibiotic_df = antibiotic_df.drop('Genus', 1)
+    antibiotic_df = antibiotic_df.drop('antibiotic', axis=1)
+    antibiotic_df = antibiotic_df.drop('Genus', axis=1)
     antibiotic_df_array = antibiotic_df.to_string(index=False, header=False)
     antibiotic_df_array_sep = antibiotic_df_array.replace(' ', ',')
     antibiotic_df_array_sep = antibiotic_df_array_sep.replace(',,,', ',')
     antibiotic_df_array_sep = antibiotic_df_array_sep.replace(',,', ',')
-    outfile_genus = f"{antibiotic}_{genus}_not_taxa_files_TEST_TEST.arff"
+    outfile_genus = f"{antibiotic}_{genus}_not_taxa_files.arff"
     att_out_genus = open(outfile_genus, "w")
     i = 'Gene'
     att_out_genus.write('@RELATION    ' + i + '\n')
@@ -172,4 +173,4 @@ excluded_genuses = ['Achromobacter', 'Acinetobacter', 'Campylobacter', 'Citrobac
 
 for antibiotic in antibiotics:
     for genus in excluded_genuses:
-        write_to_arff_except_genus(antibiotic, genus, taxonomy_info_2)
+        write_to_arff_except_genus(antibiotic, genus)
