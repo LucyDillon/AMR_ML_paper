8bd9fcf004a9281f53eca9ba8d543b7690b08979
LucyDillon <62658632+LucyDillon@users.noreply.github.com>
2023-08-18 10:22:14 +0100

    Update AMR_gene_count.sh

--- AMR_gene_count.sh
+++ AMR_gene_count.sh
@@ -5,7 +5,7 @@
 #SBATCH --array=1-167%100
 echo $SLURM_ARRAY_TASK_ID
 echo $SLURM_JOB_ID
-cd /mnt/scratch2/users/40309916
+cd /mnt/scratch2/users/********
 for i in *.txt.txt.txt;
 do cat AMRFinder_genes.txt | xargs -I % grep -c "%$" $i > $i.AMRFindercount.txt;
 done
