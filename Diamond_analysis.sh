556b617b524c9cd8cdc4bf3ace822b72a832b919
LucyDillon <62658632+LucyDillon@users.noreply.github.com>
2023-08-18 10:23:10 +0100

    Update Diamond_analysis.sh

--- Diamond_analysis.sh
+++ Diamond_analysis.sh
@@ -10,7 +10,7 @@ echo $SLURM_JOB_ID
 
 module load apps/anaconda3
 module add  diamond/0.9
-cd /mnt/scratch2/users/40309916/PATRIC_Genomes/gff_files
+cd /mnt/scratch2/users/********/PATRIC_Genomes/gff_files
 
 diamond blastp -d /mnt/scratch2/igfs-anaconda/conda-envs/eggnog/lib/python3.9/site-packages/data/eggnog_proteins.dmnd -q /mnt/scratch2/users/40309916/PATRIC_Genomes/gff_files/split_prodigal_proteinsdn --more-sensitive --threads 2 -e 0.001000 -o /mnt/scratch2/users/40309916/PATRIC_Genomes/gff_files/split_prodigal_proteinsdn_sequences.txt --top 3 --query-cover 0 --subject-cover 0
 # Version: diamond v0.9.24.125
