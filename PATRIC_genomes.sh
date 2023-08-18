5e2ff6fc4152e4274a3f583272ccc62a918917ca
LucyDillon <62658632+LucyDillon@users.noreply.github.com>
2023-08-18 10:24:33 +0100

    Update PATRIC_genomes.sh

--- PATRIC_genomes.sh
+++ PATRIC_genomes.sh
@@ -4,9 +4,7 @@
 #SBATCH --partition=medpri
 #SBATCH --job-name=WGS_sequences_gff
 #SBATCH --mail-user=ldillon05@qub.ac.uk
-#SBATCH --mail-type=BEGIN
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
-cd /mnt/scratch2/users/40309916
+#SBATCH --mail-type=BEGIN,END,FAIL
+cd /mnt/scratch2/users/********
 for i in `cat genomes.txt`; do wget -qN "ftp://ftp.patricbrc.org/genomes/$i/$i.*gff";
 done
