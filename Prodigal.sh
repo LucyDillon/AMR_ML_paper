00cbe4fb61b0cce0808720aaef9a7f6cdcbd0bea
LucyDillon <62658632+LucyDillon@users.noreply.github.com>
2023-08-18 10:25:17 +0100

    Update Prodigal.sh

--- Prodigal.sh
+++ Prodigal.sh
@@ -2,7 +2,7 @@
 #SBATCH --time=48:00:00
 #SBATCH --partition=lowpri
 #SBATCH --job-name=prodigal_run
-cd /mnt/scratch2/users/40309916/PATRIC_Genomes
+cd /mnt/scratch2/users/********/PATRIC_Genomes
 module load apps/anaconda3
 source activate /mnt/scratch/igfs-anaconda/conda-envs/prokka
 for i in *.fna; do prodigal -i $i -o $i.genes -f gff -a $i.proteins.gff; done
